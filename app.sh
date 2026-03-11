2ba58a220fb36546b812e1f65e81085948c561a0
nikhilsonawane-jpg <nikhilsonawane1611@gmail.com>
2026-03-11 18:41:45 +0530

    Create app.sh

---
+++ app.sh
@@ -0,0 +1,9 @@
+echo '<!DOCTYPE html>
+<html>
+<head>
+  <title>Jenkins Docker Demo</title>
+</head>
+<body>
+  <h1>Hello from Jenkins Docker Pipeline 🚀</h1>
+</body>
+</html>' | sudo tee /var/www/html/index.index
